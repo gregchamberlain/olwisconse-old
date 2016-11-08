4fec4ec5a3062694e3dcd05a23aa31c308a346bc
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-11-08 01:07:30 -0800

    Updated Login page

--- frontend/pages/Login.js
+++ frontend/pages/Login.js
@@ -80,8 +80,8 @@ const styles = {
     left: 0,
     width: '100%',
     height: '100%',
-    backgroundColor: '#444',
-    // backgroundImage: 'url(https://lh3.googleusercontent.com/fojDxn5BfT7eTFHwGbvpPSjlUQAUBkgzJDzOwuPaOyQZjOIL8IYjcRpOx-kIWnJ8F-AmDjWrPyCR_3DqMXVKGI2PsSQLKOgp_j7tRod41wYe9jDNWof3Z-PJ9hYK5tfioYZ3zbwOOSP7thqqV1bRMicfKFrV7CjmgCkq-EPx6Cp79cB1I4Fw44v74A5Y1wfT5pi_Hd1pRItc-lyX7KOLV4RBWuZyNZiQZ0JhubD5UrXLrCttGYARZ2offCWTgNIuckXRsgVabzvkvgYkO0JjkEGoYqBiftocS0ESWmaKhgMIj6oL38SZX-ReK_GWU6mG_6QueiY7HriqXVx932m_u0t-qbYNyV_6lvHhSD6k68TUiau9xDA5GWUFSPuyPQb96qklux0bZNfvsZ-qd7XsL8WeqllLnKGF4coYTNTRv0djxexhcJ3cslsg1oGakFtYlJP8jvadFDVb1KLASBtnbSNBZnLuH_JAf_mdO1mj-McogyGmFvyBJpolCfUvkgzOxCuChNb_pPXHT-X11W162JptQ_uoN1_S3UPXKz0Hp0X9K-ZaI5uchb4WRxbeuhtwBmKZRykakwOb7zcFnPTG5qBaKDg227lUbTcwlUL3zXhUR3qb=w997-h651-no)',
+    // backgroundColor: '#444',
+    backgroundImage: 'url(https://scontent-mia1-2.xx.fbcdn.net/v/t34.0-12/14958813_1235835326439676_2141494212_n.jpg?oh=bb78822f00c2a7cbd43570557e437223&oe=58234C99)',
     backgroundSize: 'cover',
     backgroundPosition: 'center'
   }
